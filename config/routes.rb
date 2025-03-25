2a9f14ebe7f1c7b248f114431983d7011bdae6d7
Edouard Dillee <ed.dillee@outlook.com>
2025-03-25 15:38:41 +1100

    Coin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :watch_lists, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :watch_lists_coins, only: [:create, :update, :destroy]
+  end
+
+  resources :transactions, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  resources :wallets, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :wallets_coins, only: [:index, :show]
+    resources :transactions, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  end
+
+  resources :transactions, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  resources :coins, only: [:index, :show]
 end
