3d4f37939f1f35d00801c8a1703b21cb1eea6c90
Edouard Dillee <ed.dillee@outlook.com>
2025-04-01 14:09:40 +1100

    Final transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,15 @@
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_transaction, only: [ :show, :destroy ]
   before_action :skip_authorization, only: [ :create ]
 
   def index
     @transactions = policy_scope(Transaction)
+  end
+
+  def new
+    @transaction = Transaction.new
+    @transaction.user = current_user
+    authorize @transaction
   end
 
   def show
@@ -11,154 +17,21 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    to_coin_id = transaction_params[:to_coin_id]
-    to_wallet_id = transaction_params[:to_wallet_id]
-    to_quantity = transaction_params[:to_quantity].to_f
-    new_transaction_params = transaction_params.to_h.symbolize_keys
-    new_transaction_params.delete(:to_coin_id)
-    new_transaction_params.delete(:to_wallet_id)
-    new_transaction_params.delete(:to_quantity)
-    new_transaction_params.merge!(user: current_user)
-    new_transaction_params[:quantity] = new_transaction_params[:quantity].to_f
-    new_transaction_params[:total_value] = new_transaction_params[:total_value].to_f
+    transaction_data = extract_transaction_params(transaction_params)
 
-    case new_transaction_params[:transaction_type]
-    when 'buy'
-      ActiveRecord::Base.transaction do
-        buy_transaction = Transaction.create!(new_transaction_params)
-        sell_transaction = Transaction.create!(
-          user: current_user,
-          date: buy_transaction.date,
-          wallet: buy_transaction.wallet,
-          coin: Coin.find_by(symbol: 'AUD'),
-          transaction_type: 'sell',
-          quantity: buy_transaction.total_value,
-          total_value: buy_transaction.total_value
-        )
-        TradeTransaction.create!(
-          transaction_type: "buy",
-          first_transaction: buy_transaction,
-          second_transaction: sell_transaction,
-          third_transaction: nil
-        )
-      end
-    when 'sell'
-      ActiveRecord::Base.transaction do
-        sell_transaction = Transaction.create!(new_transaction_params)
-        buy_transaction = Transaction.create!(
-          user: current_user,
-          date: sell_transaction.date,
-          wallet: sell_transaction.wallet,
-          coin: Coin.find_by(symbol: 'AUD'),
-          transaction_type: 'buy',
-          quantity: sell_transaction.total_value,
-          total_value: sell_transaction.total_value
-        )
-        TradeTransaction.create!(
-          transaction_type: "sell",
-          first_transaction: sell_transaction,
-          second_transaction: buy_transaction,
-          third_transaction: nil
-        )
-      end
-    when 'deposit'
-      ActiveRecord::Base.transaction do
-        deposit_transaction = Transaction.create!(new_transaction_params.merge(user: current_user))
-        TradeTransaction.create!(
-          transaction_type: "deposit",
-          first_transaction: deposit_transaction,
-          second_transaction: nil,
-          third_transaction: nil
-        )
-      end
-    when 'withdrawal'
-      ActiveRecord::Base.transaction do
-        withdrawal_transaction = Transaction.create!(new_transaction_params.merge(user: current_user))
-        TradeTransaction.create!(
-          transaction_type: "withdrawal",
-          first_transaction: withdrawal_transaction,
-          second_transaction: nil,
-          third_transaction: nil
-        )
-      end
-    when 'swap'
-      buy_transaction = Transaction.new(new_transaction_params)
-      buy_transaction.transaction_type = "buy"
-      ActiveRecord::Base.transaction do
-        buy_transaction.save!
-        sell_transaction = Transaction.create!(
-          user: current_user,
-          date: buy_transaction.date,
-          wallet: Wallet.find(to_wallet_id),
-          coin: Coin.find(to_coin_id),
-          transaction_type: 'sell',
-          quantity: to_quantity,
-          total_value: buy_transaction.total_value
-        )
-        TradeTransaction.create!(
-          transaction_type: "swap",
-          first_transaction: buy_transaction,
-          second_transaction: sell_transaction,
-          third_transaction: nil
-        )
-      end
-    when 'transfer'
-      ActiveRecord::Base.transaction do
-        sending_transaction = Transaction.create!(new_transaction_params)
-        receiving_transaction = Transaction.create!(
-          user: current_user,
-          date: sending_transaction.date,
-          wallet: Wallet.find(to_wallet_id),
-          coin: sending_transaction.coin,
-          transaction_type: 'transfer',
-          quantity: to_quantity
-        )
-        TradeTransaction.create!(
-          transaction_type: "transfer",
-          first_transaction: sending_transaction,
-          second_transaction: receiving_transaction,
-          third_transaction: nil
-        )
-      end
-
-    else
-      flash.now[:alert] = "Invalid transaction type"
-      render :index, status: :unprocessable_entity and return
+    begin
+      process_transaction(transaction_data)
+      redirect_to transactions_path, notice: "Transaction successfully created"
+    rescue => e
+      flash[:alert] = "Transaction creation failed: #{e.message}"
+      redirect_to transactions_path, status: :unprocessable_entity
     end
-
-    redirect_to transactions_path, notice: "Transaction successfully created"
-  rescue ActiveRecord::RecordInvalid => e
-    flash[:alert] = "Transaction creation failed: #{e.message}"
-    redirect_to transactions_path, status: :unprocessable_entity
-  end
-
-
-  def edit
-    authorize @transaction
-    render partial: "edit_modal", layout: false, locals:{transaction: @transaction}
-  end
-
-  def update
-    # set the @transaction
-    # trade_transaction = TradeTransaction.find_by(first_transaction_id: @transaction.id) || TradeTransaction.find_by(second_transaction_id: @transaction.id)
-    # @other_transaction = trade_transaction.
-    authorize @transaction
-    @transaction.update(transaction_params)
-    # @other_transaction.update(transaction_params)
-    redirect_to transactions_path(@transaction)
   end
 
   def destroy
     authorize @transaction
     @transaction.destroy
-
     redirect_to transactions_path, status: :see_other
-  end
-
-  def new
-    @transaction = Transaction.new
-    @transaction.user = current_user
-    authorize @transaction
   end
 
   private
@@ -180,5 +53,165 @@ class TransactionsController < ApplicationController
       :total_value,
       :fee
     )
+  end
+
+  def extract_transaction_params(params)
+    data = params.to_h.symbolize_keys
+
+    secondary_params = {
+      to_coin_id: data.delete(:to_coin_id),
+      to_wallet_id: data.delete(:to_wallet_id),
+      to_quantity: data.delete(:to_quantity).to_f
+    }
+
+    data[:quantity] = data[:quantity].to_f
+    data[:total_value] = data[:total_value].to_f if data[:total_value].present?
+
+    data[:user] = current_user
+
+    {
+      primary: data,
+      secondary: secondary_params
+    }
+  end
+
+  def process_transaction(data)
+    transaction_type = data[:primary][:transaction_type]
+
+    case transaction_type
+    when 'buy'
+      process_buy_transaction(data)
+    when 'sell'
+      process_sell_transaction(data)
+    when 'deposit'
+      process_deposit_transaction(data)
+    when 'withdrawal'
+      process_withdrawal_transaction(data)
+    when 'swap'
+      process_swap_transaction(data)
+    when 'transfer'
+      process_transfer_transaction(data)
+    else
+      raise "Invalid transaction type: #{transaction_type}"
+    end
+  end
+
+  def process_buy_transaction(data)
+    ActiveRecord::Base.transaction do
+      buy_transaction = Transaction.create!(data[:primary])
+
+      sell_transaction = Transaction.create!(
+        user: current_user,
+        date: buy_transaction.date,
+        wallet: buy_transaction.wallet,
+        coin: Coin.find_by(symbol: 'AUD'),
+        transaction_type: 'sell',
+        quantity: buy_transaction.total_value,
+        total_value: buy_transaction.total_value
+      )
+
+      TradeTransaction.create!(
+        transaction_type: "buy",
+        first_transaction: buy_transaction,
+        second_transaction: sell_transaction,
+        third_transaction: nil
+      )
+    end
+  end
+
+  def process_sell_transaction(data)
+    ActiveRecord::Base.transaction do
+      sell_transaction = Transaction.create!(data[:primary])
+
+      buy_transaction = Transaction.create!(
+        user: current_user,
+        date: sell_transaction.date,
+        wallet: sell_transaction.wallet,
+        coin: Coin.find_by(symbol: 'AUD'),
+        transaction_type: 'buy',
+        quantity: sell_transaction.total_value,
+        total_value: sell_transaction.total_value
+      )
+
+      TradeTransaction.create!(
+        transaction_type: "sell",
+        first_transaction: sell_transaction,
+        second_transaction: buy_transaction,
+        third_transaction: nil
+      )
+    end
+  end
+
+  def process_deposit_transaction(data)
+    ActiveRecord::Base.transaction do
+      deposit_transaction = Transaction.create!(data[:primary])
+
+      TradeTransaction.create!(
+        transaction_type: "deposit",
+        first_transaction: deposit_transaction,
+        second_transaction: nil,
+        third_transaction: nil
+      )
+    end
+  end
+
+  def process_withdrawal_transaction(data)
+    ActiveRecord::Base.transaction do
+      withdrawal_transaction = Transaction.create!(data[:primary])
+
+      TradeTransaction.create!(
+        transaction_type: "withdrawal",
+        first_transaction: withdrawal_transaction,
+        second_transaction: nil,
+        third_transaction: nil
+      )
+    end
+  end
+
+  def process_swap_transaction(data)
+    ActiveRecord::Base.transaction do
+      buy_params = data[:primary].dup
+      buy_params[:transaction_type] = "buy"
+      buy_transaction = Transaction.create!(buy_params)
+
+      sell_transaction = Transaction.create!(
+        user: current_user,
+        date: buy_transaction.date,
+        wallet: Wallet.find(data[:secondary][:to_wallet_id]),
+        coin: Coin.find(data[:secondary][:to_coin_id]),
+        transaction_type: 'sell',
+        quantity: data[:secondary][:to_quantity],
+        total_value: buy_transaction.total_value
+      )
+
+      TradeTransaction.create!(
+        transaction_type: "swap",
+        first_transaction: buy_transaction,
+        second_transaction: sell_transaction,
+        third_transaction: nil
+      )
+    end
+  end
+
+  def process_transfer_transaction(data)
+    ActiveRecord::Base.transaction do
+      sending_transaction = Transaction.create!(data[:primary])
+
+      receiving_transaction = Transaction.create!(
+        user: current_user,
+        date: sending_transaction.date,
+        wallet: Wallet.find(data[:secondary][:to_wallet_id]),
+        coin: sending_transaction.coin,
+        transaction_type: 'transfer',
+        quantity: data[:secondary][:to_quantity]
+      )
+
+      TradeTransaction.create!(
+        transaction_type: "transfer",
+        first_transaction: sending_transaction,
+        second_transaction: receiving_transaction,
+        third_transaction: nil
+      )
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   get "search", to: "search#index", as: :search
-
-
   get 'elements', to: 'pages#elements' # TODO: To be removed onces all formatting is done
 
   resources :watchlists do
@@ -15,7 +13,8 @@ Rails.application.routes.draw do
     resources :wallets_coins, only: [:index, :show]
   end
 
-  resources :transactions
+  resources :transactions, except: [:edit, :update]
+
   resources :coins, only: [:index, :show] do
     resources :messages, only: :create
   end
